ef381061e1eb73cac012f2191d54d20949ab5ead
Ricard Roberg <ricardroberg@gmail.com>
2024-04-07 14:23:03 -0300

    organiza lista por ID primeiro e depois lote

--- src/service/csvService/EscritorCSV.java
+++ src/service/csvService/EscritorCSV.java
@@ -3,24 +3,29 @@ package service.csvService;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class EscritorCSV {
     public void escreverArquivo(List<String> linhas) {
         String cabecalho = "id,nome_produto,lote,validade, quantidade";
-        linhas.add(0,cabecalho);
 
-        linhas = linhas.stream()
+        List<String> linhasFiltradas = linhas.stream()
                 .filter(linha -> !linha.endsWith(",0") && !linha.endsWith(",0\n"))
-                .toList();
+                .collect(Collectors.toList());
+
+        linhasFiltradas.sort(Comparator.comparing((String linha) -> linha.split(",")[0])
+                .thenComparing((String linha) -> linha.split(",")[2]));
+
+        linhasFiltradas.add(0,cabecalho);
 
         Path caminho = caminhoNovoArquivo();
 
 
         try {
             Files.createFile(caminho);
-            Files.write(caminho,linhas);
+            Files.write(caminho,linhasFiltradas);
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
